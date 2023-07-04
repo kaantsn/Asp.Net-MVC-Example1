c94c997591abc24c4687d32aa8e13b9e336870fc
Kaan Tosun <tr.kaantsn@gmail.com>
2023-07-04 14:09:03 +0300

    Add project files.

---
+++ Example1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Example1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Product 1", Price = 9.99m, CreatedAt = DateTime.Now },
+                new Product { Id = 2, Name = "Product 2", Price = 19.99m, CreatedAt = DateTime.Now },
+                new Product { Id = 3, Name = "Product 3", Price = 29.99m, CreatedAt = DateTime.Now }
+            };
+
+            return View(products);
+        }
+    }
+}
